48c12c56307daac52e7c311a8f756e67d7a8d99c
Ymm0008 <being0008@163.com>
2018-09-17 16:46:30 +0800

    ymm

--- group/group_event/__init__.py
+++ group/group_event/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask_debugtoolbar import DebugToolbarExtension
 from group_event.sensing.views import mod as group_event_sensingModule
 from group_event.event_search.event_search import mod as event_searchModule
+from group_event.network_analysis.views_network import mod as networkModule
 
 def create_app():
     app = Flask(__name__)
@@ -12,6 +13,7 @@ def create_app():
 
     app.register_blueprint(group_event_sensingModule)
     app.register_blueprint(event_searchModule)
+    app.register_blueprint(networkModule)
 
     return app
 
